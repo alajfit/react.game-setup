99e3aac8cc87e6d2914fa22dd305a5aab660d343
Alan Fitzpatrick <alajfit@gmail.com>
2018-02-14 10:19:44 +0000

    Added default canvas

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Canvas = () => {
+    const style = {
+        border: '1px solid black'
+    }
+    return (
+        <svg 
+            id="home-canvas"
+            preserveAspectRatio="xMaxyMax none"
+            style={style} >
+            <circle cx={0} cy={0} r={50} />
+        </svg>
+    )
+}
+
+export default Canvas
