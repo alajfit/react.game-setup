c1217bacd9b7dcb9dd4d3b49c6f8accc6ba9e138
Alan Fitzpatrick <alajfit@gmail.com>
2018-02-14 20:26:11 +0000

    Added in base components

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import Sky from './Sky'
 
 const Canvas = () => {
-    const style = {
-        border: '1px solid black'
-    }
+    const viewBox = [
+        window.innerWidth / -2,
+        100 - window.innerHeight,
+        window.innerWidth,
+        window.innerHeight
+    ]
+
     return (
         <svg 
             id="home-canvas"
             preserveAspectRatio="xMaxyMax none"
-            style={style} >
+            viewBox={viewBox} >
+
+            <Sky />
             <circle cx={0} cy={0} r={50} />
         </svg>
     )
