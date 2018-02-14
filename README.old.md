04913d4d9d6008e07548aed20a9f0b47b291ca17
Alan Fitzpatrick <alajfit@gmail.com>
2018-02-14 10:04:12 +0000

    Init Redux

--- README.old.md
+++ README.old.md
@@ -1,6 +1,9 @@
 # react.game-setup
 A simple game built in React
 
+## Commands that can be run
+
+```javascript
   npm start
     Starts the development server.
 
@@ -13,3 +16,14 @@ A simple game built in React
   npm run eject
     Removes this tool and copies build dependencies, configuration files
     and scripts into the app directory. If you do this, you can’t go back!
+```
+
+## Reference Material
+
+### Redux
+- https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
+- https://auth0.com/blog/redux-practical-tutorial/
+
+### SVG
+- https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial
+- https://www.sarasoueidan.com/blog/svg-coordinate-systems/
--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.props.message}</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+App.PropTypes = {
+  message: PropTypes.string.isRequired
+}
+
+export default App
